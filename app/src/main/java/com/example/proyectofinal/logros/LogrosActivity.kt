e79b3048142b64057838afe3e072c941cfbc5b39
cjpd1 <cpintodaza@gmail.com>
2026-06-05 21:41:59 -0500

    pantallita logrosss

--- app/src/main/java/com/example/proyectofinal/logros/LogrosActivity.kt
+++ app/src/main/java/com/example/proyectofinal/logros/LogrosActivity.kt
@@ -1,4 +1,16 @@
 package com.example.proyectofinal.logros
 
-class LogrosActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.proyectofinal.R
+
+class LogrosActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logros)
+
+        findViewById<android.view.View>(R.id.btnVolverLogros).setOnClickListener {
+            finish()
+        }
+    }
 }
